7f80c40389bfcba43bb64937e855873a6bdcd602
kazei1211 <94268065+kazei1211@users.noreply.github.com>
2021-11-18 23:15:09 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+if __name__ == '__main__':
+	print('Hello')
+	print('   **           **')
+	print(' ********   ********')
+	print('*********************')
+	print(' *******************')
+	print('   **************')
+	print('     **********')
+	print('        *****')
+	print('          *')
+	print('Happy teacher"s day 20/11')
