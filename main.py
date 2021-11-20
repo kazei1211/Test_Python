834a2e04346f39132037a20148c227de21b2c418
Phạm Nguyễn <94268065+kazei1211@users.noreply.github.com>
2021-11-20 21:14:41 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,3 @@ if __name__ == '__main__':
 	print('Hello wolrd_2')
 	print('Hello wolrd_3')
 	print('Hello wolrd_4')
-#	   **           **
-#	 ********   ********
-#	*********************
-#	 *******************
-#	   **************
-#	     **********
-#	        *****
-#	          *
-#	Happy teacher"s day 20/11
